5a1ec46c7c69000e0fde0fafdc16afb24e8532ad
miktokki <mikktok@gmail.com>
2026-01-28 20:06:35 +0200

    First commit

---
+++ src/main/java/bookstore/bookstore/model/Book.java
@@ -0,0 +1,10 @@
+package bookstore.bookstore.model;
+
+public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+    private int price;
+
+}
